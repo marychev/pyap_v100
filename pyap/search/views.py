65f08795ea342a3d46353cd92aaa4f713db1f678
marychev <marychev_mihail@mail.ru>
2018-05-01 15:52:05 +0300

    update all pyap****

--- pyap/search/views.py
+++ pyap/search/views.py
@@ -1,6 +1,8 @@
 from django.views.generic.base import TemplateView
 from generic.mixins import MainPageMixin
 from product.models import Product
+from page.models import Page
+from blog.models import Post
 from simple_search import search_form_factory
 from utils.pagination import get_pagination
 
@@ -12,18 +14,32 @@ class Search(MainPageMixin, TemplateView):
     def get_context_data(self, **kwargs):
         # todo: add to forms.py
         SearchForm = search_form_factory(
-            Product.objects.filter(is_show=True), 
-            ['title', 'articul',])
+            Product.objects.filter(is_show=True), ['title', 'articul'])
 
         context = super(Search, self).get_context_data(**kwargs)
         context['q'] = self.request.GET.get('q', None)
         context['title'] = 'Поиск по запросу: `{}`'.format(context['q'])
         context['form'] = SearchForm(self.request.GET or {})
-        context['objects'] = Product.objects.none()
+        # context['objects'] = Product.objects.none()
 
+        context['objects'] = []
         if context['form'].is_valid() and context['q']:
-            context['objects'] = get_pagination(self.request, context['form'].get_queryset())
+            # дополнительные модели, которые должны учавствовать в поиске
+            q = context['q']
+            pages = Page.objects.filter(is_show=True, title__icontains=q)
+            if pages.exists():
+                context['objects'].append(pages)
+            posts = Post.objects.filter(is_show=True, title__icontains=q)
+            if posts.exists():
+                context['objects'].append(posts)
+            products = Product.objects.filter(is_show=True, title__icontains=q)
+            if products.exists():
+                context['objects'].append(products)
         return context
+
+        # if context['form'].is_valid() and context['q']:
+        #     context['objects'] = get_pagination(self.request, context['form'].get_queryset())
+        # return context
 
 # ------------
 # HELP search
